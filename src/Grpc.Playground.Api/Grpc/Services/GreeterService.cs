171310c027e04539dc96b659b4349cb23d29ec5c
William Baldoumas <itabaiyu@gmail.com>
2021-03-13 16:41:06 -0500

    Project Setup

---
+++ src/Grpc.Playground.Api/Grpc/Services/GreeterService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace Grpc.Playground.Api.Grpc.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        public override async Task<GreeterResponse> Greet(GreeterRequest request, ServerCallContext context) =>
+            await Task.FromResult(new GreeterResponse { Message = $"Hello {request.Name}!" });
+
+        public override async Task<GreeterResponse> GreetMany(
+            IAsyncStreamReader<GreeterRequest> requestStream,
+            ServerCallContext context)
+        {
+            var requests = new List<GreeterRequest>();
+
+            while (await requestStream.MoveNext())
+            {
+                requests.Add(requestStream.Current);
+            }
+
+            return new GreeterResponse
+            {
+                Message = $"Hello {string.Join(", ", requests.Select(r => r.Name))}"
+            };
+        }
+
+        public override async Task GreetManyTimes(
+            GreeterRequest request,
+            IServerStreamWriter<GreeterResponse> responseStream,
+            ServerCallContext context)
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                await responseStream.WriteAsync(new GreeterResponse
+                {
+                    Message = $"Hello {request.Name} {i}!"
+                });
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        public override async Task GreetOnTheFly(
+            IAsyncStreamReader<GreeterRequest> requestStream,
+            IServerStreamWriter<GreeterResponse> responseStream,
+            ServerCallContext context)
+        {
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                await responseStream.WriteAsync(
+                    new GreeterResponse
+                    {
+                        Message = $"Hello {request.Name}!"
+                    }
+                );
+            }
+        }
+    }
+}
---
+++ src/Grpc.Playground.Api/Startup.cs
@@ -0,0 +1,55 @@
+using Grpc.Playground.Api.Grpc.Interceptors;
+using Grpc.Playground.Api.Grpc.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Grpc.Playground.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddGrpc().AddServiceOptions<GreeterService>(options =>
+            {
+                options.Interceptors.Add<MetricLoggingInterceptor>();
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Grpc.Playground.Api", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Grpc.Playground.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGrpcService<GreeterService>();
+            });
+        }
+    }
+}
